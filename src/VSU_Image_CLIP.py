9a7ba32ef1426e8f3a203b7efa205d0cde2c7450
tako <tako@pop-os.localdomain>
2023-12-28 01:46:47 +0900

    first commit

---
+++ src/VSU_Image_CLIP.py
@@ -0,0 +1,59 @@
+import torch
+from PIL import Image
+import open_clip
+
+from VectorSearchBase import VectorSearchBase
+
+
+class ImageVectorSearchUtil(VectorSearchBase):
+    def __init__(self, db_name=':memory:'):
+        super(ImageVectorSearchUtil, self).__init__(db_name)
+
+
+    # override
+    def init_model(self):
+        self.tokenizer = open_clip.get_tokenizer('ViT-B-32')
+        self.model, _, self.preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+        self.vec_size = self.model.token_embedding.embedding_dim
+
+
+    # override
+    def do_zeroshot(self):
+        if self.zeroshot_vec is None:
+            return
+
+        image_features = torch.tensor(self.data["vector"])
+        text_features = torch.tensor(self.zeroshot_vec)
+
+        scores = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+        pred = []
+        for s in scores:
+            idx = s.tolist().index(max(s))
+            pred.append(self.zeroshot_labels[idx])
+
+        self.data["zeroshot_pred"] = pred
+        return scores, pred
+
+
+    # override
+    def _trans_vec_main_func(self, ar):
+        imgs = [Image.open(p) for p in ar]
+        images = [self.preprocess(img).unsqueeze(0) for img in imgs]
+        image_features = [self.model.encode_image(image) for image in images]
+        image_features = [vec / vec.norm(dim=-1, keepdim=True) for vec in image_features]
+        image_features = [vec.tolist()[0] for vec in image_features]
+
+        # image_features if type(image_features) == type([]) else
+        tmp = image_features
+        return image_features
+
+
+    # override
+    def _trans_vec_sub_func(self, ar):
+        text = self.tokenizer(ar)
+        text_features = self.model.encode_text(text)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+
+        v = text_features.tolist()
+        return v
---
+++ src/VSU_Text_E5.py
@@ -0,0 +1,68 @@
+import torch.nn.functional as F
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+
+from VectorSearchBase import VectorSearchBase
+
+
+class VectorSearchUtil(VectorSearchBase):
+    def __init__(self, db_name=':memory:'):
+        super(VectorSearchUtil, self).__init__(db_name)
+
+
+    # override
+    def init_model(self):
+        model_name = 'intfloat/multilingual-e5-small'
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+
+        self.vec_size = self.model.embeddings.word_embeddings.embedding_dim
+
+
+    def __average_pool(self, last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
+
+    # override
+    def do_zeroshot(self):
+        if self.zeroshot_vec is None:
+            return
+
+        v = F.normalize(Tensor(self.data["vector"]), p=2, dim=1)
+
+        z = np.array(F.normalize(Tensor(self.zeroshot_vec), p=2, dim=1))
+        scores = (v @ z.T) * 100
+
+        pred = []
+        for s in scores:
+            idx = s.tolist().index(max(s))
+            pred.append(self.zeroshot_labels[idx])
+
+        self.data["zeroshot_pred"] = pred
+        return scores, pred
+
+
+    # override
+    def _trans_vec_main_func(self, ar):
+        ar = ['query: '+a for a in ar]
+        batch_dict = self.tokenizer(ar, max_length=512, padding=True, truncation=True, return_tensors='pt')
+        outputs = self.model(**batch_dict)
+        embeddings = self.__average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+
+        features = embeddings.tolist()
+
+        return features
+
+
+    # override
+    def _trans_vec_sub_func(self, ar):
+        return self._trans_vec_main_func(ar)
+
+
+    # override
+    def query(self, q, k=5):
+        q = 'query: '+q
+        return super().query(q, k)
+
